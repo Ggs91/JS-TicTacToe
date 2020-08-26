e5b6049858f3309116906923f715efa2cc3a9b8c
Ggs91 <georges_atalla@hotmail.fr>
2020-08-26 12:30:32 +0200

    Board module complet

--- main.js
+++ main.js
@@ -22,3 +22,62 @@ function BoardCase(caseID, doc = document){
     id: caseID, //caseID est une variable set dans la scope de ma factory car c'est un paramètre à elle. Là, je peut la rendre accessible au public sou s la pripriété "id"
   }
 }
+
+const Board = (function(){ //Board est un module et non une simple factory function. On a besoin que d'une seule board dans le jeu. 1 seule closure sera crée donc. Il faut lancer un nouveau game pour réinitilisé unenouvelle board.
+  const boardCases = (function(gridSize){ //Instanciation des BoardeCases via une IIFE qui set dans la variable "boardCases" le resultat de la fonction.
+    const boardCasesIDs = generateBoardCasesIDsArray(gridSize);
+    return boardCasesIDs.map(caseID => BoardCase(caseID))
+  })(3) //Ici on choisi la taille de la grille (ici 3x3)
+
+  const winningCombinationsIDs = [
+    ["a1", "b1", "c1"],
+    ["a2", "b2", "c2"],
+    ["a3", "b3", "c3"],
+    ["a1", "a2", "a3"],
+    ["b1", "b2", "b3"],
+    ["c1", "c2", "c3"],
+    ["a1", "b2", "c3"],
+    ["a3", "b2", "c1"],
+  ]
+
+  function generateBoardCasesIDsArray(gridSize){ //Méthode qui génère tous les noms des cases en fonction de la taille de la grille qu'on veut.
+    const arrayOfCases = [];
+    const gridColumnID = Array.from({ length: gridSize }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i)); //["a", "b", "c"...]
+    const gridRowsID = [...Array(gridSize + 1).keys()].slice(1); //["1", "2", "3"...]
+
+    gridColumnID.forEach(columnID => {
+      gridRowsID.forEach(rowID => {
+        arrayOfCases.push(columnID + rowID);
+      })
+    })
+
+    return arrayOfCases; //["a1", "a2", "a3", "b1"...]
+  }
+
+  function findCaseByID(boardID){
+    return boardCases.find(boardCase => boardCase.id === boardID);
+  }
+
+  function hasWinningCombination(){
+    return winningCombinationsIDs.some(combinationIDsarray => {
+      return combinationIDsarray.every(caseID => findCaseByID(caseID).caseContent === "X") || combinationIDsarray.every(caseID => findCaseByID(caseID).caseContent === "O");
+    })
+  }
+
+  function isFull(){
+    return boardCases.every(boardcase => boardcase.caseContent)
+  }
+
+  function hasAnEndingCondition(){
+    return  hasWinningCombination() || isFull();
+  }
+
+  function clear(){
+    boardCases.forEach(boardcase => boardcase.caseContent = "")
+  }
+  return {
+    hasAnEndingCondition,
+    clear,
+    boardCases,
+  }
+})()
